3c0761b5bcec35cb18cf432952b9e7597442a337
Apostolos GALANOPOULOS <Apostolos.GALANOPOULOS@netcompany.com>
2024-02-05 12:14:14 +0200

    FIX: hide QR code in case the mobile flow, change the link 'auth' to button style, and align on the middle of the panel

--- src/app/features/home/services/home.service.ts
+++ src/app/features/home/services/home.service.ts
@@ -5,25 +5,20 @@ import { MenuOption } from '../models/menu-option';
 export class HomeService {
 
 	options: MenuOption[] = [
-  //   {
-	// 	key: 'SIOP',
-	// 	value: 'Online Authentication  (SIOP)',
-	// 	isDisabled: false,
-	// },
-	{
-		key: 'OID4VP_CBOR_Selectable',
-		value: 'OID4VP + CBOR selectable',
-		isDisabled: false,
-	},
-	{
-		key: 'OID4VP_CBOR',
-		value: 'OID4VP + CBOR',
-		isDisabled: false,
-	},
-	{
-		key: 'OID4VP_C',
-		value: 'OID4VP Custom',
-		isDisabled: false,
-	}
+		{
+			key: 'OID4VP_CBOR_Selectable',
+			value: 'OID4VP + CBOR selectable',
+			isDisabled: false,
+		},
+		{
+			key: 'OID4VP_CBOR',
+			value: 'OID4VP + CBOR',
+			isDisabled: false,
+		},
+		{
+			key: 'OID4VP_C',
+			value: 'OID4VP Custom',
+			isDisabled: false,
+		}
 	];
 }
