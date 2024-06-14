74d18adaec980d825c3e5c76f2df975fa0252f27
vafeini <129304399+vafeini@users.noreply.github.com>
2024-06-14 15:40:24 +0300

    Get first namespace (#61)
    Co-authored-by: Vafeiadis Nikos <nvafeiadis@netcompany.com>

--- src/app/core/services/cbor/cbor-decode.service.ts
+++ src/app/core/services/cbor/cbor-decode.service.ts
@@ -1,50 +1,56 @@
-import { Injectable } from '@angular/core';
-import { from, Observable, mergeMap, forkJoin} from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Buffer } from 'buffer';
+import {Injectable} from '@angular/core';
+import {forkJoin, from, mergeMap, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {Buffer} from 'buffer';
 import bdec from 'cbor-bigdecimal';
 import * as cbor from 'cbor-web';
-import { KeyValue } from '@angular/common';
-import { TagValue } from '@app/core/models/CBOR';
+import {KeyValue} from '@angular/common';
+import {TagValue} from '@app/core/models/CBOR';
 
 bdec(cbor);
 
 @Injectable()
 export class CborDecodeService {
 
-	decode (vpToken: string): Observable<KeyValue<string, string>[]> {
-		const buf = Buffer.from(vpToken, 'base64');
-		return from(cbor.decodeFirst(buf, {
-			bigint: true,
-			preferWeb: true
-		}))
-			.pipe(
-				map((valueOut: any) => valueOut.documents[0].issuerSigned.nameSpaces.entries().next().value ),
-				map((dataArray: TagValue[]) => {
-					const requestData: any[] = [];
-					dataArray.forEach((item: TagValue) => {
-						requestData.push(this.cborDecode(item.value));
-					});
-					return requestData;
-				}),
-				map((valueOut: any) => valueOut),
-				mergeMap(
-					(data: Observable<any>) =>
-						forkJoin(data)
-							.pipe(
-								map(
-									(items) =>
-										items.map((item: { elementIdentifier: string, elementValue: string}) =>
-										{ return { key: item.elementIdentifier.replaceAll('_', ' '), value: item.elementValue}; })
-								),
-							)
-				),
-			);
-	}
-	cborDecode (buf: Uint8Array): Observable<any> {
-		return from(cbor.decodeFirst(buf, {
-			bigint: true,
-			preferWeb: true
-		}));
-	}
+  decode(vpToken: string): Observable<KeyValue<string, string>[]> {
+    const buf = Buffer.from(vpToken, 'base64');
+    return from(cbor.decodeFirst(buf, {
+      bigint: true,
+      preferWeb: true
+    }))
+      .pipe(
+        map((valueOut: any) => {
+          let namespaces = valueOut.documents[0].issuerSigned.nameSpaces;
+          let firstNamespace = Object.entries(namespaces)[0][0];
+          return namespaces[firstNamespace];
+        }),
+        map((dataArray: TagValue[]) => {
+          const requestData: any[] = [];
+          dataArray.forEach((item: TagValue) => {
+            requestData.push(this.cborDecode(item.value));
+          });
+          return requestData;
+        }),
+        map((valueOut: any) => valueOut),
+        mergeMap(
+          (data: Observable<any>) =>
+            forkJoin(data)
+              .pipe(
+                map(
+                  (items) =>
+                    items.map((item: { elementIdentifier: string, elementValue: string }) => {
+                      return {key: item.elementIdentifier.replaceAll('_', ' '), value: item.elementValue};
+                    })
+                ),
+              )
+        ),
+      );
+  }
+
+  cborDecode(buf: Uint8Array): Observable<any> {
+    return from(cbor.decodeFirst(buf, {
+      bigint: true,
+      preferWeb: true
+    }));
+  }
 }
