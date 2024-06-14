706e9c6485832a2469a3333fc5f8120a6b66ebd7
Vafeiadis Nikos <nvafeiadis@netcompany.com>
2024-06-14 14:12:37 +0300

    Update PID msoMdoc namespace to be aligned with ARF 1.4

--- src/app/core/data/pid_age_over_18_pd.ts
+++ src/app/core/data/pid_age_over_18_pd.ts
@@ -7,7 +7,7 @@ export const PID_AGE_OVER_18_PD: Presentation = {
 		'id': '32f54163-7166-48f1-93d8-ff217bdb0653',
 		'input_descriptors': [
 			{
-				'id': 'eu.europa.ec.eudiw.pid.1',
+				'id': 'eu.europa.ec.eudi.pid.1',
 				'name': 'EUDI PID',
 				'purpose': 'We need to verify you are over 18 using your PID',
 				'format': {
@@ -23,7 +23,7 @@ export const PID_AGE_OVER_18_PD: Presentation = {
 					'fields': [
 					  {
 							'path': [
-								"$['eu.europa.ec.eudiw.pid.1']['age_over_18']"
+								"$['eu.europa.ec.eudi.pid.1']['age_over_18']"
 							],
 							'intent_to_retain': false
 						}
