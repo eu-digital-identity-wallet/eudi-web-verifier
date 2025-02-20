8c6159af03efb9e2cb8ae24fdb6fe98718b9fe66
Georgios DIMTSAS <georgios.dimtsas@netcompany-intrasoft.com>
2025-02-20 15:56:31 +0200

    Add a note indicating the compatible openid4vp draft that is implemented by the verifier.

--- src/app/core/layout/wallet-layout/wallet-layout.component.ts
+++ src/app/core/layout/wallet-layout/wallet-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WalletLayoutHeaderComponent } from './wallet-layout-header/wallet-layout-header.component';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'vc-wallet-layout',
@@ -9,5 +10,5 @@ import { WalletLayoutHeaderComponent } from './wallet-layout-header/wallet-layou
     styleUrls: ['./wallet-layout.component.scss']
 })
 export class WalletLayoutComponent {
-
+    environment = environment;
 }
