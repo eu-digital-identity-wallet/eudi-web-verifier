952eb7e845bc50fb32042154f937fd8e37d61404
Georgios Dimtsas <gedim21@gmail.com>
2025-08-14 12:16:19 +0300

    Display presented documents per dcql query

--- src/app/features/invoke-wallet/components/presentations-results/presentations-results.component.ts
+++ src/app/features/invoke-wallet/components/presentations-results/presentations-results.component.ts
@@ -11,9 +11,8 @@ import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatDialog, MatDialogModule} from "@angular/material/dialog";
 import {OpenLogsComponent} from "@shared/elements/open-logs/open-logs.component";
-import {Observable, of} from "rxjs";
-import {map} from "rxjs/operators";
-import { PresentationQuery } from '@app/core/models/TransactionInitializationRequest';
+import {Observable} from "rxjs";
+import {PresentationQuery} from '@app/core/models/TransactionInitializationRequest';
 
 @Component({
     selector: 'vc-presentations-results',
@@ -38,32 +37,13 @@ export class PresentationsResultsComponent implements OnInit {
 
   @Input() concludedTransaction!: ConcludedTransaction;
   presentationQuery!: PresentationQuery;
-  attestations$: Observable<(Single | Errored)[]> = of([]);
+  attestationsPerQuery: {[queryId: string]: Observable<(Single | Errored)[]>} = {}
+  
   readonly dialog: MatDialog = inject(MatDialog);
 
   ngOnInit(): void {
     this.presentationQuery = this.concludedTransaction.presentationQuery;
-    this.attestations$ = this.responseProcessor.mapVpTokenToAttestations(this.concludedTransaction)
-        .pipe(
-          map((attestations) => {
-            return this.flatten(attestations)
-          })
-        );
-  }
-
-  flatten(sharedAttestations: PresentedAttestation[]): (Single | Errored)[] {
-    let singles: (Single | Errored)[] = []
-    sharedAttestations.forEach(it => {
-      switch (it.kind) {
-        case "enveloped":
-          return singles.push(...it.attestations)
-        case "single":
-          return singles.push(it)
-        case "error":
-          return singles.push(it)
-      }
-    })
-    return singles
+    this.attestationsPerQuery = this.responseProcessor.mapVpTokenToAttestations(this.concludedTransaction);
   }
 
   isErrored(it: Single | Errored): it is Errored {
