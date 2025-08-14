4906809ca468bf0e4dd3643e7172da42e5bfcb34
Giorgos Dimtsas <gedim21@gmail.com>
2025-08-14 13:51:35 +0300

    Remove AGE_OVER_18 attestation definitions and references (#196)

--- src/app/core/models/attestation/AttestationType.ts
+++ src/app/core/models/attestation/AttestationType.ts
@@ -2,7 +2,6 @@ export enum AttestationType {
   PID = "pid",
   MDL = "mdl",
   PHOTO_ID = "photo_id",
-  AGE_OVER_18 = "age_over_18",
   EHIC = "ehic",
   EHIC_DC4EU = "ehic-dc4eu",
   PDA1 = "pda1"
