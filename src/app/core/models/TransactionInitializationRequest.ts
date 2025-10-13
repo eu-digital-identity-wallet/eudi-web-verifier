65de0d0aa0d751a6f58ebedf1f05cdb530437e47
Giorgos Dimtsas <gedim21@gmail.com>
2025-10-13 14:20:16 +0300

    Remove 'type' field from TransactionInitializationRequest (#199)

--- src/app/core/models/TransactionInitializationRequest.ts
+++ src/app/core/models/TransactionInitializationRequest.ts
@@ -1,7 +1,6 @@
 import { DCQL } from './dcql/DCQL';
 
 export type TransactionInitializationRequest = {
-  type: 'id_token' | 'vp_token' | 'vp_token id_token';
   nonce: string;
   request_uri_method: 'get' | 'post';
   dcql_query: DCQL;
