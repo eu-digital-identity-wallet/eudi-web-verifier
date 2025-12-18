0b19baaf07bca3c27829b8bb59ab948305b2f5f2
Georgios Dimtsas <gedim21@gmail.com>
2025-12-18 14:31:28 +0200

    Refactor presentation options component layout and styles for improved clarity and usability

--- src/app/features/presentation-request-preparation/components/presentation-options/presentation-options.component.ts
+++ src/app/features/presentation-request-preparation/components/presentation-options/presentation-options.component.ts
@@ -4,7 +4,12 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { Profile, RequestUriMethod } from '@app/core/models/TransactionInitializationRequest';
+import { MatCardModule, MatCardHeader, MatCardSubtitle, MatCard, MatCardTitle, MatCardContent } from '@angular/material/card';
+
+import {
+  Profile,
+  RequestUriMethod,
+} from '@app/core/models/TransactionInitializationRequest';
 
 @Component({
   selector: 'vc-presentation-options',
@@ -14,9 +19,14 @@ import { Profile, RequestUriMethod } from '@app/core/models/TransactionInitializ
     MatButtonToggleModule,
     MatFormFieldModule,
     MatInputModule,
-  ],
+    MatCardHeader,
+    MatCard,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent
+],
   templateUrl: './presentation-options.component.html',
-  styleUrl: './presentation-options.component.scss'
+  styleUrl: './presentation-options.component.scss',
 })
 export class PresentationOptionsComponent {
   @Input() presentationProfileControl!: FormControl<Profile>;
