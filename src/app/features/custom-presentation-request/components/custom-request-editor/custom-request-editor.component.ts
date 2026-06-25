852a1b2a9f1f48fdc96c9f335358e77e6fcdea8b
DIMTSAS Georgios <dimtsas@netcompany.com>
2026-06-25 11:57:42 +0300

    Updated custom request to support both submission channels

--- src/app/features/custom-presentation-request/components/custom-request-editor/custom-request-editor.component.ts
+++ src/app/features/custom-presentation-request/components/custom-request-editor/custom-request-editor.component.ts
@@ -1,32 +1,53 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
 import { DataService } from '@app/core/services/data-service';
 import { SharedModule } from '@app/shared/shared.module';
 
 @Component({
-    selector: 'vc-presentation-request-editor',
-    imports: [CommonModule, SharedModule],
-    templateUrl: './custom-request-editor.component.html',
-    styleUrls: ['./custom-request-editor.component.scss']
+  selector: 'vc-presentation-request-editor',
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    SharedModule,
+    MatButtonToggleModule,
+  ],
+  templateUrl: './custom-request-editor.component.html',
+  styleUrls: ['./custom-request-editor.component.scss'],
 })
 export class CustomRequestEditorComponent {
+  invalidJSON = true;
+  constructor(private readonly dataService: DataService) {}
 
-	invalidJSON = true;
-	constructor (
-    private readonly dataService: DataService
-	) {}
+  requestObjectJson: string = '';
+  channel: 'redirects' | 'dc-api' = 'redirects';
 
-	onRequest (code: string) {
-		this.invalidJSON = code.length === 0 || this.isJSON(code);
-    this.dataService.customRequest$.next(code);
-	}
+  onRequestChanged(requestObjectJson: string) {
+    this.requestObjectJson = requestObjectJson;
+    this.invalidJSON =
+      requestObjectJson.length === 0 || this.isJSON(requestObjectJson);
+    this.dataService.customRequest$.next({
+      requestObjectJson: requestObjectJson,
+      channel: this.channel,
+    });
+  }
 
-  isJSON (requestCode: string) {
+  onChannelChanged(channel: 'dc-api' | 'redirects') {
+    this.channel = channel;
+    this.dataService.customRequest$.next({
+      requestObjectJson: this.requestObjectJson,
+      channel: channel,
+    });
+  }
+
+  isJSON(requestCode: string) {
     try {
-      return (JSON.parse(requestCode) && !!requestCode);
+      return JSON.parse(requestCode) && !!requestCode;
     } catch (e) {
       return false;
     }
   }
-
 }
